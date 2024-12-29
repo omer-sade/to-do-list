23a8e84a8144d1504c5342229aa885d8ee5fcbc1
omer-sade <108121786+omer-sade@users.noreply.github.com>
2024-12-29 20:30:53 +0200

    Delete node_modules directory

--- node_modules/@cloudscape-design/components/internal/hooks/use-modal-component-analytics/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const useModalContextLoadingButtonComponent: (isPrimaryButton: boolean, loading: boolean) => void;
-export declare const useModalContextLoadingComponent: () => void;
-//# sourceMappingURL=index.d.ts.map
--- node_modules/@cloudscape-design/components/live-region/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { LiveRegionProps } from './interfaces';
-export { LiveRegionProps };
-declare function LiveRegion({ assertive, hidden, tagName, ...restProps }: LiveRegionProps): JSX.Element;
-export default LiveRegion;
-//# sourceMappingURL=index.d.ts.map
